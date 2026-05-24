89507ba3f0416e2900a801225d2f09b091b3ff3b
Bhavya <smartkidbhavya@gmail.com>
2026-05-23 23:46:55 -0400

    chapter 2 boilerplate + repo_path + repo_dir

---
+++ libwyag.py
@@ -0,0 +1,60 @@
+import argparse
+import configparser
+from datetime import datetime
+try:
+    import grp, pwd
+except ModuleNotFoundError:
+    pass
+from fnmatch import fnmatch
+import hashlib
+
+from math import ceil
+import os
+import re
+import sys
+import zlib
+
+argparser = argparse.ArgumentParser(description="The stupidest content tracker")
+
+argsubparsers = argparser.add_subparsers(title="Commands", dest="command")
+argsubparsers.required = True
+
+
+def main(argv=sys.argv[1:]):
+    args = argparser.parse_args(argv)
+    match args.command:
+        case "add"          : cmd_add(args)
+        case "cat-file"     : cmd_cat_file(args)
+        case "check-ignore" : cmd_check_ignore(args)
+        case "checkout"     : cmd_checkout(args)
+        case "commit"       : cmd_commit(args)
+        case "hash-object"  : cmd_hash_object(args)
+        case "init"         : cmd_init(args)
+        case "log"          : cmd_log(args)
+        case "ls-files"     : cmd_ls_files(args)
+        case "ls-tree"      : cmd_ls_tree(args)
+        case "rev-parse"    : cmd_rev_parse(args)
+        case "rm"           : cmd_rm(args)
+        case "show-ref"     : cmd_show_ref(args)
+        case "status"       : cmd_status(args)
+        case "tag"          : cmd_tag(args)
+        case _              : print("Bad command.")
+
+def repo_path(repo, *args):
+    return os.path.join(repo.gitdir, *args)
+
+def repo_dir(repo, *args, mkdir = False):
+    current_path = repo_path(repo,*args)
+    if (os.path.exists(current_path)):
+        if (os.path.isdir(current_path)):
+            return current_path
+        else:
+            raise TypeError(f"Not a directory {current_path}")
+    elif(mkdir == True):
+        os.makedirs(current_path)
+        return current_path
+    else:
+        return None
+
+def repo_file(repo, *path, mkdir=False):
+    
